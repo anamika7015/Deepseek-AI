e70471fa352e7bde9c4e19ff017772a2e85217ee
Anamika Pandey <149082695+anamika7015@users.noreply.github.com>
2025-03-31 15:54:39 +0530

    commit 2

--- deepseek/app/page.jsx
+++ deepseek/app/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { assets } from "@/assets/assets";
+import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
  import { useState } from "react";
 export default function Home() {
@@ -9,7 +10,8 @@ export default function Home() {
   return (
       <div >
         <div className="flex h-screen ">
-          {/* sidebar */}
+         <Sidebar expand={expand} setExpand={setExpand}/>
+
           <div className="flex-1 flex flex-col item-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
             <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
               <Image onClick={()=> (expand ? setExpand(false): setExpand(true))} 
