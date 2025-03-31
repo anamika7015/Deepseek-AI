a28e49cc6243a0318cbb0d9d9bf6715195ac63be
Anamika Pandey <149082695+anamika7015@users.noreply.github.com>
2025-03-31 15:28:43 +0530

    day6

--- deepseek/components/Sidebar.jsx
+++ deepseek/components/Sidebar.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 const Sidebar = ({expand, setExpand}) => {
   return (
-    <div>
+    <div className= {`flex flex-col justify-between bg-[#212317] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
       <div>
-        <div>
+        <div className={`flex ${expand ? "flex-row gap-10" : "flex-col items-center gap-8"}`}>
             <Image src={expand ? assets.logo_text : assets.logo_icon} alt=''/>
             <div>
             <Image src={assets.menu_icon } alt='' className=' md:hidden'/>
-            <Image src={expand ? assets.logo_text : assets.logo_icon} alt=''/>
+            <Image src={expand ? assets.sidebar_close_icon: assets.sidebar_icon} alt='' className='md:block hidden w-7'/>
             </div>
         </div>
       </div>
